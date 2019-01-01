f12cd1b0d7d532e23b64d53a58b4eb4b1529ac4a
leihua <leihua@masslottery.com>
2019-01-01 12:14:20 -0500

    check authentication

--- company/views.py
+++ company/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 
 def home(request):
+    user = request.user
+    if request.user.is_authenticated:
+        user = request.user.first_name
     template = 'home.html'
-    context = {}
+    context = {
+        "user": user
+    }
     return render(request, template, context)
