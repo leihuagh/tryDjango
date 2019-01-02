013d81cfcda7d88f884c1616a062fde24ac3cb6f
leihua <leihua@masslottery.com>
2019-01-02 17:17:04 -0500

    set company as project level app

--- company/urls.py
+++ company/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from .views import (
+    home,
+    about,
+    contact,
+)
 
-app_name = 'company'
 urlpatterns = [
-
+    path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
 ]
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -15,15 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from company.views import (
-    home,
-    about,
-    contact,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
+    path('', include('company.urls')),
 ]
